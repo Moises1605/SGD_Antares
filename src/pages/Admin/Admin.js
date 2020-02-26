c445db68dd4e1ffaf90546dd9801c13072ffb169
Roberto Maia <romaiajr5@gmail.com>
2020-02-26 12:54:49 -0300

    Sidebar Working

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -1,91 +1,68 @@
-import React from 'react';
-import {Container, Row, Col, Nav} from 'react-bootstrap';
-import Bolsistas from './Bolsistas';
-import Funcionario from './Funcionario';
-import Relatorio from './Relatorio';
-import Escolas from './Escolas';
-import LateralBar from './lateral_bar';
-import Navbar from './NavBar'
-import Sidebar from './Sidebar';
+import React from "react";
+import { Container, Row, Col, Nav } from "react-bootstrap";
+import Bolsistas from "./Bolsistas";
+import Funcionario from "./Funcionario";
+import Relatorio from "./Relatorio";
+import Escolas from "./Escolas";
+import Navbar from "./NavBar";
+import SimpleList from "./List";
+import "./Admin.css";
 
-export default class Admin extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            screens : [
-                <Relatorio/>,
-                <Funcionario/>,
-                <Bolsistas/>,
-                <Escolas/>,
-                null,
-            ],
-            active :null,
-        };
-        this.handleClickLink = this.handleClickLink.bind(this);
-        this.form = this.form.bind(this);
-    }
-    
-    handleClickLink(eventKey){
-        this.setState({
-            active: this.state.screens[eventKey-1]
-        });	
-    }
+export default class Admin extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      screens: [
+        <Relatorio />,
+        <Funcionario />,
+        <Bolsistas />,
+        <Escolas />,
+        null
+      ],
+      active: null
+    };
+  }
+  componentDidMount = screen => {
+    this.setState({ active: this.state.screens[screen] });
+  };
 
-    form(eventKey){
-        this.setState({
-            active: this.state.screens[eventKey-1]
-        });	
-    }
-    render(){
-        return(
-            <div>
-                <Container fluid style={{paddingLeft: 0, paddingRight: 0}}>
-                    <Row>
-                        <Container fluid>
-                            <Navbar/>
-                        </Container> 
-                    </Row>
-                    <Row>
+  render() {
+    return (
+      <div id="div_admin">
+        <Container fluid style={{ paddingLeft: 0, paddingRight: 0 }}>
+          <Row>
+            <Col>
+              <Container fluid>
+                <Navbar />
+              </Container>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div id="div_sidebar">
+                <SimpleList
+                  screens={this.state.screen}
+                  onClick={this.componentDidMount}
+                />
+              </div>
+            </Col>
+            <Col>
+              <div id="div_adminContent">
+                <Container fluid>
+                  <Row>
                     <Col>
-                    <div style={{
-                        height: "100vh",
-                        width : "18vw",
-                        float: "left",
-                        borderStyle: "solid",
-                        borderWidth: " 0px 1px 0px 0px",
-                        borderColor: "#ececec",
-                        backgroundColor: "#f8f9fa",
-			position: "fixed",
-			paddingTop: "20vh"
-                    }}>
-                            <LateralBar onClick={this.handleClickLink} />
-                        </div>
+                      <div style={{ height: "5vh" }}></div>
                     </Col>
-                    <Col>
-                        <div style={{
-                            float: "right",
-                            height: "90vh",
-                            width: "78vw",
-                            paddingTop: "15vh",
-                            paddingLeft: "2vw",
-                        }}>
-                            <Container fluid >
-                                <Row>
-                                    <Col>
-                                        <div style={{height: "5vh"}}></div>
-                                    </Col>
-                                </Row>
-                                <Row>	    
-                                    <Col>
-                                        {this.state.active}
-                                    </Col>
-                                </Row>
-                            </Container>
-                        </div>
-                        </Col>
-                    </Row>
+                  </Row>
+                  <Row>
+                    <Col>{this.state.active}</Col>
+                  </Row>
                 </Container>
-            </div>
-        )
-    }
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
--- src/pages/Admin/List.js
+++ src/pages/Admin/List.js
@@ -1,77 +1,74 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Divider from '@material-ui/core/Divider';
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import PeopleIcon from '@material-ui/icons/People';
-import SchoolIcon from '@material-ui/icons/School';
-import ImportContactsIcon from '@material-ui/icons/ImportContacts';
-import BackupIcon from '@material-ui/icons/Backup';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import React, { Component } from "react";
+import { makeStyles, withTheme } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import PeopleIcon from "@material-ui/icons/People";
+import SchoolIcon from "@material-ui/icons/School";
+import ImportContactsIcon from "@material-ui/icons/ImportContacts";
+import BackupIcon from "@material-ui/icons/Backup";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 //import logo from '../../assets/logo.png';
 
+class SimpleList extends Component {
+  handleClick = teste => {
+    this.setState({ screen: teste });
+  };
 
+  render() {
+    return (
+      <div id="div_side">
+        <div id="side_content">
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItem button onClick={() => this.props.onClick("0")}>
+              <ListItemIcon>
+                <AssessmentIcon />
+              </ListItemIcon>
+              <ListItemText primary="Exibir Relatórios" />
+            </ListItem>
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
-export default function SimpleList(props) {
-  const classes = useStyles();
-
-  function ListItemLink(props) {
-    return <ListItem button component="a" {...props} />;
+            <ListItem button onClick={() => this.props.onClick("1")}>
+              <ListItemIcon>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Gerir Funcionários" />
+            </ListItem>
+            <ListItem button onClick={() => this.props.onClick("2")}>
+              <ListItemIcon>
+                <SchoolIcon />
+              </ListItemIcon>
+              <ListItemText primary="Gerir Bolsistas" />
+            </ListItem>
+            <ListItem button onClick={() => this.props.onClick("3")}>
+              <ListItemIcon>
+                <ImportContactsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Gerir Escolas" />
+            </ListItem>
+            <ListItem button onClick={() => this.props.onClick("4")}>
+              <ListItemIcon>
+                <BackupIcon />
+              </ListItemIcon>
+              <ListItemText primary="Realizar Backup" />
+            </ListItem>
+          </List>
+        </div>
+        <div id="side_encerrar">
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <ExitToAppIcon />
+              </ListItemIcon>
+              <ListItemText primary="Sair" />
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    );
   }
-
-  return (
-    <div className={classes.root}>
-      <List component="nav" aria-label="main mailbox folders" onClick={console.log("funciona")}> 
-        <ListItemLink eventKey="1" >
-          <ListItemIcon>
-            <AssessmentIcon />
-          </ListItemIcon>
-          <ListItemText primary="Exibir Relatórios" />
-        </ListItemLink>
-        <ListItemLink eventKey="2">
-          <ListItemIcon>
-            <PeopleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Gerir Funcionários"/>
-        </ListItemLink>
-        <ListItemLink eventKey="3">
-          <ListItemIcon>
-            <SchoolIcon />
-          </ListItemIcon>
-          <ListItemText primary="Gerir Bolsistas" />
-        </ListItemLink>
-        <ListItemLink eventKey="4">
-          <ListItemIcon>
-            <ImportContactsIcon/>
-          </ListItemIcon>
-          <ListItemText primary="Gerir Escolas" />
-        </ListItemLink>
-        <ListItemLink eventKey="5">
-          <ListItemIcon>
-            <BackupIcon />
-          </ListItemIcon>
-          <ListItemText primary="Realizar Backup" />
-        </ListItemLink>
-      </List>
-      <List id="encerrar">
-      <ListItem button>
-          <ListItemIcon>
-            <ExitToAppIcon />
-          </ListItemIcon>
-          <ListItemText primary="Sair" />
-        </ListItem>
-      </List>
-    </div>
-  );
 }
+
+export default SimpleList;
