491717f616b77a3f16fc0a0a6a25bf3a7d5791ff
DanielDouradoFsa <60206918+DanielDouradoFsa@users.noreply.github.com>
2020-02-27 09:18:32 -0300

    Atualizando nome do endereço

--- src/pages/Admin/form_bolsista.js
+++ src/pages/Admin/form_bolsista.js
@@ -135,7 +135,7 @@ class CadastroBolsista extends Component {
                 <Form.Control
                   required
                   placeholder="Rua, Bairro, Número."
-                  value={this.state.adress}
+                  value={this.state.address}
                   className="address"
                   onChange={this.handleChange}
                 />
