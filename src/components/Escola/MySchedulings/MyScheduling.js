77214324d1d015576c444d0297b4d9e46edae679
Moises1605 <56726033+Moises1605@users.noreply.github.com>
2020-03-20 20:59:18 -0300

    visualização de visitas

--- src/components/Escola/MySchedulings/MyScheduling.js
+++ src/components/Escola/MySchedulings/MyScheduling.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./style.css";
-import { Card, Button, Accordion } from "react-bootstrap";
+import { Card, Button, Row, Col, InputGroup, FormControl, Form, Alert, Modal } from "react-bootstrap";
 import api from '../../../services/api';
+import DateRangeIcon from '@material-ui/icons/DateRange';
 //Tela onde a escola poderá vê os seus agendamentos.
 export default class MyScheduling extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      schedulings: ["a", "b", "c"],
+      schedulings: [{ data: "25/02", Responsável: "Ana", horario: "09:30" }, { data: "20/02", Responsável: "Samuel", horario: "11:30" }, { data: "25/09", Responsável: "Alice", horario: "15:30" }],
       idSchooll: this.props.idSchool,
-      controle:0
+      controle: false,
+      current: {}
     };
+    this.setControl = this.setControl.bind(this);
   }
 
   componentDidMount() {
@@ -24,50 +27,133 @@ export default class MyScheduling extends React.Component {
     //api.get("/cancelarvisita", this.state.id);
   }
 
-  update(){
-    this.setState({controle: (this.state.controle + 1)})
+  setControl(event) {
+    console.log(event.target.name);
+    var teste = event.target.name;
+    this.setState({ current: this.state.schedulings[teste] })
+    this.setState({ controle: true })
   }
 
   render() {
     return (
       <div>
-        <h1 id="titleScheduling">Meus Agendamentos</h1>
+        <Modal
+          size="lg"
+          show={this.state.controle}
+          onHide={() => this.setState({ controle: false })}
+          aria-labelledby="example-modal-sizes-title-lg"
+          id='modal'
+        >
+          <Modal.Header closeButton id='header'>
+            <Modal.Title id="example-modal-sizes-title-lg">
+              Agendamento de visita
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group as={Row} controlId="formHorizontalEmail">
+                <Form.Label column sm={3}>
+                  Nome do responsável
+                </Form.Label>
+                <Col sm={6}>
+                  <Form.Control type="text" placeholder="Nome completo" value={this.state.current.Responsável} />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="formHorizontalStudent">
+                <Form.Label column sm={3}>
+                  Quantidade de alunos
+                            </Form.Label>
+                <Col sm={3}>
+                  {/* <Form.Control type="text" placeholder="Max: 40" value={this.state.students} /> */}
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="formHorizontalDate">
+                <Form.Label column sm={3}>
+                  Horário
+                </Form.Label>
+                <Col sm={3}>
+                  <Form.Control Type="text" value={this.state.current.horario} />
+                </Col>
+              </Form.Group>
+
+              <Form.Group as={Row} controlId="formHorizontalSerie">
+                <Form.Label column sm={3}>
+                  Série(Ano)
+                </Form.Label>
+                <Col sm={2}>
+                  {/* <Form.Control type="text" placeholder=" " value={this.state.number} /> */}
+                </Col>
+              </Form.Group>
+            </Form>
+            <span>
+              Status:
+              <Alert variant="success">
+                Confirmado
+              </Alert>
+            </span>
+          </Modal.Body>
+        </Modal>
+        <div id="head">
+          <Row>
+            <Col>
+              <h1 id="titleScheduling">Meus Agendamentos</h1>
+            </Col>
+            <Col>
+              <InputGroup>
+                <FormControl
+                  placeholder="Digite a data da visita..."
+                  value={this.state.search}
+                  onChange={this.handleChange}
+                />
+                <InputGroup.Prepend>
+                  <Button variant="outline-secondary">&#128269;</Button>
+                </InputGroup.Prepend>
+              </InputGroup>
+            </Col>
+          </Row>
+        </div>
         <div id="list">
-          <Accordion defaultActiveKey="0">
-            {this.state.schedulings.map(item => (
-              // {this.state.days.indexOf(item).toString()}
-              
-              <Card key={item.id} id="item">
-                <Card.Header>
-                  <Accordion.Toggle as={Button} variant="link" eventKey={this.state.schedulings.indexOf(item)}>
-                    Click me!
-                  </Accordion.Toggle>
-                </Card.Header>
-                <Accordion.Collapse eventKey={this.state.schedulings.indexOf(item)}>
-                  <Card.Body>Hello! I'm the body</Card.Body>
-                </Accordion.Collapse>
-              </Card>       
-            ))}
-          </Accordion>
+          {this.state.schedulings.map(item => (
+            // {this.state.days.indexOf(item).toString()}
+            <Card id='item' >
+              <Card.Body>
+                <span>
+                  <span id='test'>
+                    <DateRangeIcon fontSize="large" />
+                  </span>
+                  <span id='data'>
+                    {item.data}
+                  </span>
+                  <span id="responsavel">
+                    Responsável:{item.Responsável}
+                  </span>
+                  <span id='button'>
+                    <Button
+                      variant="outline-primary"
+                      id="view"
+                      name = {this.state.schedulings.indexOf(item)}
+                      onClick={this.setControl}
+                    >
+                      Visualizar
+                    </Button>
+
+                    <Button
+                      variant="outline-danger"
+                      id="cancel2"
+                      onClick={this.cancelScheduling(item)}
+                    >
+                      Cancelar visita
+                    </Button>
+                  </span>
+                </span>
+              </Card.Body>
+            </Card>
+
+          ))}
         </div>
       </div>
     );
   }
 }
-
-function update(){
-  
-}
-// {/* <Card.Title>{`Data:${item.data}`}</Card.Title>
-//                                 <Card.Subtitle className="mb-2 text-muted">{`Horário:${item.horario}`}</Card.Subtitle>
-//                                 <Card.Text>{`Responsável ${item.responsavel}`}</Card.Text>
-//                                 <Card.Text>{`Temas: ${item.temas}`}</Card.Text>
-//                                 <Card.Text>{`Quantidade de Alunos: ${item.alunos}`}</Card.Text>
-//                                 <Card.Text>{`Situação: ${item.situação}`}</Card.Text>*/}
-// <Button
-//   variant="outline-danger"
-//   id="cancel"
-//   onClick={this.cancelScheduling(item.id)}
-// >
-//   Cancelar visita
-//                   </Button>
