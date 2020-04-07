3d1a881cf637dc3373f2a87a659deb14ce6a228e
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-04-06 22:11:29 -0300

    States de Permissões

--- src/components/Admin/form_funcionario.js
+++ src/components/Admin/form_funcionario.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Form, Container, Row, Col, Button } from "react-bootstrap";
 import api from "../../services/api";
 import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
 
 export default class CadastroFuncionario extends Component {
   constructor() {
@@ -19,6 +21,15 @@ export default class CadastroFuncionario extends Component {
       phone: "", //telefone
       password: "", //senha
       repassword: "", //confirmar senha
+      gerirB: false, //gerir bolsista
+      gerirF: false, //gerir Funcionário
+      validarA: false, //validar agendamento
+      gerarR: false, //gerar relatorio
+      inserirA: false, //inserir Ativadade extra
+      gerirHor: false, //gerir Horário
+      gerirBackup: false, //gerir Backup
+      gerirE: false, //gerir escolas
+      id: "",
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -30,15 +41,28 @@ export default class CadastroFuncionario extends Component {
     this.setState({ [event.target.name]: value });
   }
 
+  handleCheck = (event) => {
+    this.setState({ [event.target.name]: event.target.checked });
+  };
+
   async handleSubmit(event) {
-    api.post("/adicionarFuncionario", this.state);
-    event.PreventDefault();
+    var result = api.post("/adicionarFuncionario", this.state);
+    this.setState({ id: result });
+    api.post("/addPermissoes", this.state);
   }
 
   disableButton = () => {
     return this.state.repassword === ""
       ? false
-      : this.state.password === this.state.repassword
+      : this.state.password === this.state.repassword &&
+        (this.state.gerirB !== false ||
+          this.state.gerarR !== false ||
+          this.state.gerirBackup !== false ||
+          this.state.gerirF !== false ||
+          this.state.gerirHor !== false ||
+          this.state.validarA !== false ||
+          this.state.inserirA !== false ||
+          this.state.gerirE !== false)
       ? false
       : true;
   };
@@ -48,6 +72,11 @@ export default class CadastroFuncionario extends Component {
       <Container>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group controlledId="Name-surname">
+            <Form.Row>
+              <Form.Label>
+                <h6>Insira os dados do Funcionário</h6>
+              </Form.Label>
+            </Form.Row>
             <Form.Row>
               <Col>
                 {/*<Form.Label>Nome</Form.Label>
@@ -383,6 +412,116 @@ export default class CadastroFuncionario extends Component {
                   ></TextField>
                 </div>
               </Col>
+            </Form.Row>
+            <br />
+            <Form.Row>
+              <Form.Label>
+                <h6>Selecione as permissões do usuário.</h6>
+              </Form.Label>
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    size="small"
+                    color="primary"
+                    name="gerirB"
+                  />
+                }
+                label="Gerenciar Bolsistas"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="gerirHor"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Cadastrar Horário dos Bolsistas"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="gerirF"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Gerenciar Funcionários"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="inserirA"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Inserir Novas Atividades"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="gerirE"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Gerenciar Escolas"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="validarA"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Confirmar Agendamentos"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="gerirBackup"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Realizar e Restaurar Backups"
+              />
+            </Form.Row>
+            <Form.Row>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    onChange={this.handleCheck}
+                    name="gerarR"
+                    size="small"
+                    color="primary"
+                  />
+                }
+                label="Criar Relatório"
+              />
             </Form.Row>
           </Form.Group>
           <Form.Row>
